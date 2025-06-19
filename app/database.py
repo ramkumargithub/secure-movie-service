f14e96b9646fe5aa7a6c3f57887bf4bb7f6be7b1
Ramkumar Patturaj <ramkumar.patturaj@gmail.com>
2025-06-19 16:26:53 +0300

    Create database.py

---
+++ app/database.py
@@ -0,0 +1,9 @@
+def get_movie_details(name, year, language):
+    # mock response
+    return {
+        "name": name,
+        "year": year,
+        "language": language,
+        "summary": "A dummy movie for demo purposes",
+        "rating": "8.2"
+    }
