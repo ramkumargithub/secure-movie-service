84439ca13e606fc12c6823aa6572281c2f72cddb
Ramkumar Patturaj <ramkumar.patturaj@gmail.com>
2025-06-19 16:31:35 +0300

    Update security.py

---
+++ app/security.py
@@ -0,0 +1,10 @@
+from starlette.responses import Response
+
+def add_security_headers(response: Response) -> Response:
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
+    response.headers["X-XSS-Protection"] = "1; mode=block"
+    response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
+    response.headers["Referrer-Policy"] = "no-referrer"
+    return response
